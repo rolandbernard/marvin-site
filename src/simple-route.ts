e4db3e7e9ddc43a1bbf01c8d8c6dd2c0bd25f507
Roland Bernard <rolli.bernard@gmail.com>
2021-08-07 19:05:20 +0200

    Added a simple router

---
+++ src/simple-route.ts
@@ -0,0 +1,55 @@
+
+import { customElement, html, LitElement, property } from 'lit-element';
+
+@customElement('simple-route')
+export class SimpleRoute extends LitElement {
+
+    @property()
+    route: string = '/';
+
+    visible = false;
+
+    constructor() {
+        super();
+        this.updateRoute = this.updateRoute.bind(this);
+    }
+
+    connectedCallback() {
+        super.connectedCallback();
+        window.addEventListener('hashchange', this.updateRoute);
+        this.updateRoute();
+    }
+
+    disconnectedCallback() {
+        super.disconnectedCallback();
+        window.removeEventListener('hashchange', this.updateRoute);
+    }
+
+    updateRoute() {
+        const regEx = new RegExp(`^${this.route}$`);
+        let path;
+        if (this.route[0] === "#") {
+            path = window.location.hash || "#/";
+        } else {
+            path = window.location.pathname || "/";
+        }            
+        const match = path.match(regEx);
+        if(match && !this.visible) {
+            this.visible = true;
+            this.requestUpdate();
+        }
+        if (!match && this.visible) {
+            this.visible = false;
+            this.requestUpdate();
+        }
+    }
+
+    render() {
+        if (this.visible) {
+            return html`<slot></slot>`;
+        } else {
+            return html`<slot name="fallback"></slot>`;
+        }
+    }
+}
+

e685203efce79a9c58efec31ae0c1e771c01982c
Roland Bernard <rolli.bernard@gmail.com>
2021-08-10 00:21:07 +0200

    Added the new demo video

--- src/pages/main-page.ts
+++ src/pages/main-page.ts
@@ -8,13 +8,21 @@ export class MainPage extends LitElement {
 
     static get styles() {
         return css`
+            :host {
+                max-width: 90%;
+                width: 800px;
+            }
+            .video {
+                max-width: 100%;
+            }
         `
     }
 
     render() {
         return html`
             <video
-                class="preview"
+                class="video"
+                .defaultPlaybackRate="${1.25}"
                 ?autoplay="${true}"
                 ?loop="${true}"
                 src="${Demo}"

622cbd838256cba78cd9fa55cc6b13c5f817798c
Roland Bernard <rolli.bernard@gmail.com>
2021-08-10 23:48:15 +0200

    Implemented the footer

--- src/page-footer.ts
+++ src/page-footer.ts
@@ -1,18 +1,86 @@
 
-import { customElement, html, css, LitElement } from 'lit-element';
+import { customElement, html, css, LitElement, property } from 'lit-element';
+
+import 'material-icon';
+
+@customElement('info-button')
+export class InfoButton extends LitElement {
+
+    @property()
+    icon: string = '';
+
+    @property()
+    text: string = '';
+
+    @property()
+    href: string = '';
+
+    static get styles() {
+        return css`
+            .link {
+                color: white;
+                text-decoration: none;
+                padding: 1rem;
+                display: flex;
+                align-items: center;
+                user-select: none;
+                transition: var(--transition);
+                transition-property: opacity;
+                position: relative;
+                opacity: 0.625;
+            }
+            .link:hover {
+                opacity: 0.9;
+            }
+            .icon {
+                font-size: 1rem;
+            }
+            .name {
+                margin-left: 0.25rem;
+                font-size: 0.8rem;
+            }
+        `;
+    }
+
+    render() {
+        return html`
+            <a class="link" href="${this.href}" ondragstart="return false" @click="${() => this.requestUpdate()}">
+                <material-icon class="icon" name="${this.icon}"></material-icon>
+                <span class="name">
+                    ${this.text}
+                </span>
+            </a>
+        `;
+    }
+}
 
 @customElement('page-footer')
 export class PageFooter extends LitElement {
 
     static get styles() {
         return css`
+            .footer {
+                display: flex;
+                flex-flow: row;
+                justify-content: center;
+                align-items: center;
+            }
         `;
     }
 
     render() {
         return html`
             <div class="footer">
-                Hello World
+                <info-button
+                    icon="github"
+                    text="GitHub"
+                    href="https://github.com/rolandbernard/marvin"
+                ></info-button>
+                <info-button
+                    icon="question_answer"
+                    text="Feedback"
+                    href="https://github.com/rolandbernard/marvin/issues"
+                ></info-button>
             </div>
         `;
     }
